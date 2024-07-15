aeb35bf5661c720c9e50d24da846e697ecb4329c
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-07-15 11:50:48 +0100

    first version

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, Alert, TextInput, ScrollView, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, TextInput, ScrollView, FlatList, TouchableOpacity, ActivityIndicator, ImageBackground, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback, Platform, Modal } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
@@ -7,6 +7,7 @@ export default function App() {
   const [inputFocused, setInputFocused] = useState(false);
   const fruits = ['apple', 'guava', 'orange', 'paw-paw', 'avacado pear', 'pine-apple', 'lemon',]
   const [loader, setLoader] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false)
 
   const data = [
     {name: 'David', age: 20, id: 1},
@@ -20,27 +21,61 @@ export default function App() {
     </View>
   )
 
-  return (
-    <View style={styles.container}>
+
+  const renderHeader = () => (
+    <>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View>
+            <TextInput
+              style={{ borderColor: inputFocused ? 'blue' : 'black', borderWidth: 1, margin: 10 }}
+              onChangeText={setName}
+              value={name}
+              placeholder='Enter name'
+              onFocus={() => setInputFocused(true)}
+              onBlur={() => setInputFocused(false)}
+            />
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
       
       {loader ? <ActivityIndicator size="large" color="#00ff00" /> : null }
       <Button title='Toggle Loader' onPress={() => setLoader(!loader)} />
-      <Image style={{height: 100, width: 80, resizeMode: 'cover'}} source={require('./assets/profile.png')} />
-      <Text>Hello World!!</Text>
+      <Image style={{ height: 100, width: 80, resizeMode: 'cover' }} source={require('./assets/profile.png')} />
+      <Modal visible={modalOpen} onRequestClose={() => setModalOpen(false)}>
+       <Text>Hello World!!</Text>
+      </Modal>
+      <TouchableOpacity onPress={() => setModalOpen(true)}>
+        <Text>Open Modal</Text>
+      </TouchableOpacity>
+      <Button title='Close Modal' onPress={() => setModalOpen(false)} />
+    </>
+  );
+
+  const renderFooter = () => (
+    <>
+      <ImageBackground source={require('./assets/bg.jpg')}>
       <Button onPress={() => Alert.alert('button clicked')} title='Click' />
-      <TextInput style={{borderColor: inputFocused ? 'blue' : 'black', borderWidth: 1}} onChangeText={setName} value={name} placeholder='Enter name' onFocus={() => setInputFocused(true)} onBlur={() => setInputFocused(false)} />
-      
-      <FlatList data={data} renderItem={renderItem} keyExtractor={item => item.id.toString()} /> 
-        <TouchableOpacity onPress={() => Alert.alert('TouchableOpacity clicked')}>
-          <Text>Click</Text>
-        </TouchableOpacity>
-      <ScrollView>
-        {fruits.map(item => (
-          <Text style={{fontSize: 50, lineHeight: 100, textAlign: 'center'}}>
-            {item}
-          </Text>
-        ))}
-      </ScrollView>
+        <ScrollView style={styles.overlay}>
+          {fruits.map(item => (
+            <Text key={item} style={{ fontSize: 50, lineHeight: 100, textAlign: 'center', color: 'white' }}>
+              {item}
+            </Text>
+          ))}
+        </ScrollView>
+      </ImageBackground>
+    </>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        ListHeaderComponent={renderHeader}
+        ListFooterComponent={renderFooter}
+      />
       <StatusBar style='auto' />
     </View>
   );
@@ -50,7 +85,7 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     paddingTop: 100,
-    paddingBottom: 100,
+    paddingBottom: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -62,5 +97,10 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 20,
+    borderRadius: 10,
   },
 });
